6791505754f708680397a36d0e84ed93c9e00f84
iswiss <immanuel.schweizer@outlook.com>
2017-04-27 07:04:01 +0200

    commit solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The idea is to use a matrix inverse function already available in R so I don't have to code one myself
+## I import the MASS package which gives a matrix inverse function (ginv()) as described here
+## http://stackoverflow.com/questions/11995832/inverse-of-matrix-in-r
+require(MASS)
 
-## Write a short comment describing this function
+## First function will take a matrix and store it in data
+## It also provides functions to store and retrieve the inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Second function will take the cached matrix object
+## It will either take the cached inverse or calculate the inverse using ginv()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- ginv(data, ...)
+        x$setinverse(m)
+        m
 }
